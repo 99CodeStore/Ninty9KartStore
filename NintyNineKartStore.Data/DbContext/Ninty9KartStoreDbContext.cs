434a1bef143301663a2ec351150645d2f355df99
Bheemsen Singh <bheem_stn@hotmail.com>
2022-01-31 10:40:57 +0530

    CategoryDtos are added for varias CRUD operations.

--- NintyNineKartStore.Data/DbContext/Ninty9KartStoreDbContext.cs
+++ NintyNineKartStore.Data/DbContext/Ninty9KartStoreDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using NintyNineKartStore.Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,8 @@ namespace NintyNineKartStore.Data
         {
 
         }
+
+        public DbSet<Category> Categories;
 
     }
 }
--- NintyNineKartStore.Service/Models/CategoryDto.cs
+++ NintyNineKartStore.Service/Models/CategoryDto.cs
@@ -2,18 +2,27 @@
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
-namespace NintyNineKartStore.Core.Entities
+namespace NintyNineKartStore.Service.Models
 {
-    public class Category
+    public class CreateCategoryDto
     {
-        [Key]
-        public int Id { get; set; }
-        [Required(ErrorMessage ="Category Name is required!!")]
+        [Required(ErrorMessage = "Category Name is required!!")]
         [StringLength(20, MinimumLength = 3, ErrorMessage = "The Category Name should be between 5 to 15 characters")]
         public string Name { get; set; }
         [DisplayName("Display Order")]
         [Range(1, 500, ErrorMessage = "Display Order must be between 1 to 500 only!!")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedOn { get; set; } = DateTime.Now;
+    }
+   
+    public class UpdateCategoryDto : CreateCategoryDto
+    {
+
+    }
+
+    public class CategoryDto : CreateCategoryDto
+    {
+        [Required]
+        public int Id { get; set; }
     }
 }
